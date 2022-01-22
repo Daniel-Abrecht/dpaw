534a2243cf862162aa5bcbf1cf0c9e5e75ddd11c
Daniel Abrecht <public@danielabrecht.ch>
2022-01-22 15:09:52 +0000

    Fix special characters in rendered text

--- wm/src/font.c
+++ wm/src/font.c
@@ -6,8 +6,8 @@ int dpaw_font_init(Display* display){
   char** missing_charset_list_return;
   int missing_charset_count_return;
   char* def_string_return;
-  dpaw_font.fixed  = XCreateFontSet(display, "fixed", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
-  dpaw_font.normal = XCreateFontSet(display, "-*-times-medium-r-*-*-*-100-*-*-*-*-*-*", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
-  dpaw_font.bold   = XCreateFontSet(display, "-*-times-bold-r-*-*-*-100-*-*-*-*-*-*", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
+  dpaw_font.fixed  = XCreateFontSet(display, "fixed,-*-*-medium-r-*-*-*-100-*-*-*-*-*-*", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
+  dpaw_font.normal = XCreateFontSet(display, "-*-times-medium-r-*-*-*-100-*-*-*-*-*-*,-*-*-medium-r-*-*-*-100-*-*-*-*-*-*", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
+  dpaw_font.bold   = XCreateFontSet(display, "-*-times-bold-r-*-*-*-100-*-*-*-*-*-*,-*-*-medium-r-*-*-*-100-*-*-*-*-*-*", &missing_charset_list_return, &missing_charset_count_return, &def_string_return);
   return 0;
 }
--- wm/src/main.c
+++ wm/src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <locale.h>
 
 static struct dpaw dpaw;
 
@@ -14,6 +15,7 @@ int main(int argc, char* argv[]){
   fflush(stdout);
   setlinebuf(stdout); // If redirected to a file, we'll lose output otherwise, even the line below won't work !?!?
   setlinebuf(stderr);
+  setlocale(LC_CTYPE, "C.UTF-8"); // Xutf8DrawString & Xutf8TextExtents break if it's set to anything non-utf8, so let's enforce it...
   puts("Starting dpaw!");
   if(dpaw_init(&dpaw) == -1)
     return 1;
